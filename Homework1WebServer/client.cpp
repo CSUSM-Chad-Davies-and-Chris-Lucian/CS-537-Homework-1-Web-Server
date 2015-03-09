e629ec217f2d6ad24f6f6386285ef7a20030d3af
Chris Lucian <Chris@DeadlyApps.com>
2015-03-09 12:04:24 -0700

    Started commenting

--- Homework1WebServer/client.cpp
+++ Homework1WebServer/client.cpp
@@ -1,10 +1,14 @@
+//Chris Lucian & Chad Davies
+//3/8/2015
+//CS 537
+//Programming Assignment 1 Web Server
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include "WebServer.h"
 #include "WebClient.h"
-#include <pthread.h>
 #include <string>
 #include <iostream>
 #include <ctime>
@@ -14,99 +18,115 @@
 
 using namespace std;
 
+// Function declarations
 void Call_Client(string ipAddress, string portNumber, string version);
-void Call_Telnet(string ipAddress, string portNumber, string version);
+timespec diff(timespec start, timespec end);
 
-struct connectionParams{
-    string IPAddress;
-    string PortNumber;
-};
-
-//int clock_gettime(clockid_t clk_id, struct timespect *tp);
-
-timespec diff(timespec start, timespec end)
-{
-	timespec temp;
-	if ((end.tv_nsec-start.tv_nsec)<0) {
-		temp.tv_sec = end.tv_sec-start.tv_sec-1;
-		temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
-	} else {
-		temp.tv_sec = end.tv_sec-start.tv_sec;
-		temp.tv_nsec = end.tv_nsec-start.tv_nsec;
-	}
-	return temp;
-}
-
+//This program is a client meant to performance test the web server
+//The arguments are the ipAddress of the server and the servers port
 int main(int argc, char *argv[]) {
 
+    //Argument Validation
     if (argc < 3) {
        fprintf(stderr,"usage %s <hostname> <port>\n", argv[0]);
        exit(0);
     }
 
-    pthread_t server_thread, client_thread;
+    //Assign the arguments to well-named variables
     string ipAddress = argv[1];
     string portNumber = argv[2];
 
+    //Get the start time of the HTTP 1.0 stress test run
     timespec start;
     clock_gettime(CLOCK_REALTIME, &start);
 
+    //Call the client functions for the HTTP 1.0 protocol
     Call_Client(ipAddress, portNumber, "1.0");
 
+    //Get the end time of the HTTP 1.0 stress test run
     timespec end;
     clock_gettime(CLOCK_REALTIME, &end);
 
+    //Get the start time of the HTTP 1.1 stress test run
     timespec start2;
     clock_gettime(CLOCK_REALTIME, &start2);
 
+    //Call the client functions for the HTTP 1.1 protocol
     Call_Client(ipAddress, portNumber, "1.1");
 
+    //Get the end time of the HTTP 1.1 stress test run
     timespec end2;
     clock_gettime(CLOCK_REALTIME, &end2);
 
+    //Print the results of the stress test in (Seconds:Nanoseconds)
     cout<< "Protocol 1.0: " <<diff(start,end).tv_sec<<":"<<diff(start,end).tv_nsec<<endl;
     cout<< "Protocol 1.1: " <<diff(start2,end2).tv_sec<<":"<<diff(start2,end2).tv_nsec<<endl;
 
     return 0;
 }
 
-void Call_Telnet(string ipAddress, string portNumber, string version)
+//Get the difference between 2 times
+timespec diff(timespec start, timespec end)
 {
-  string command = "telnet " + ipAddress + " " + portNumber;
-
-  system(command.c_str());
-  //system("GET / HTTP/1.0");
+	timespec temp;
+  //If end Nanoseconds is greater than start Nanoseconds
+	if ((end.tv_nsec-start.tv_nsec)<0) {
+    //Adjust seconds and Nanoseconds to accomodate the offset
+		temp.tv_sec = end.tv_sec-start.tv_sec-1;
+		temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+	} else {
+    //otherwise do a simple subtraction
+		temp.tv_sec = end.tv_sec-start.tv_sec;
+		temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+	}
+	return temp;
 }
 
+//This function creates a web client and attempts to run 100 calls against the server
 void Call_Client(string ipAddress, string portNumber, string version)
 {
+  //Create the client
   printf("CLIENT: Client Connecting to Web Server at IP:%s Port:%s", ipAddress.c_str(), portNumber.c_str());
   WebClient* aclient = new WebClient();
+
+  //Connect to the specified server
   printf("\nCLENT: Connecting To Server\n");
   aclient->Connect(ipAddress, portNumber, version);
-  printf("\nCLENT: Sending Get Request\n");
 
+  //Loop 10 times over the 10 requests
+  printf("\nCLENT: Starting stress test\n");
   for(int i = 0; i < 10; i++)
   {
+    //duratin set to 0 however when set above 0 this can be used for debugging
     int duration = 0;
 
     printf ("\nNum of Runs: %d\n", i);
+
+    //Get the headder for the index page
     aclient->SendRequest("HEAD / HTTP/" + version);
     usleep(duration);
 
+    //Get the header for the large file
     aclient->SendRequest("HEAD /large_file.html HTTP/" + version);
     usleep(duration);
 
+    //Attempt to get the header for a missing file
     aclient->SendRequest("HEAD /missing_file.html HTTP/" + version);
     usleep(duration);
 
+    //Get the index file
     aclient->SendRequest("GET / HTTP/" + version);
     usleep(duration);
+
+    //Get a large file of lorem ipsum
     aclient->SendRequest("GET /large_file.html HTTP/" + version);
     usleep(duration);
+
+    //Attempt a get request
     aclient->SendRequest("GET /missing_file.html HTTP/" + version);
     usleep(duration);
 
+    //Build a put statement with file contents and send request
     string put_request = "PUT /stuff.html HTTP/" + version + "\n";
     put_request += "User-Agent: SomeClient 1.0\n";
     put_request += "Host: "+ ipAddress+":" + portNumber + "\n";
@@ -117,15 +137,19 @@ void Call_Client(string ipAddress, string portNumber, string version)
     aclient->SendRequest(put_request);
     usleep(duration);
 
+    //Get the file that was created by the put command
     aclient->SendRequest("GET /stuff.html HTTP/" + version);
     usleep(duration);
+    //Delete the file that was created by the put command
     aclient->SendRequest("DELETE /stuff.html HTTP/" + version);
     usleep(duration);
-    aclient->SendRequest("GET /stuff.html HTTP/" + version);
 
+    //Get the file that was deleted to verify it was deleted.
+    aclient->SendRequest("GET /stuff.html HTTP/" + version);
     usleep(duration);
   }
+
+  //Delete the pointer to the client
   printf("\nCLENT: Request Finished. Destroying Client\n");
   delete aclient;
-  //aclient->~WebClient();
 }
--- Homework1WebServer/server.cpp
+++ Homework1WebServer/server.cpp
@@ -1,3 +1,8 @@
+//Chris Lucian & Chad Davies
+//3/8/2015
+//CS 537
+//Programming Assignment 1 Web Server
+
 #include<stdio.h>
 #include<string.h>
 #include<sys/socket.h>
@@ -15,155 +20,233 @@
 #include <vector>
 #include <boost/lexical_cast.hpp>
 #include <time.h>
-using namespace std;
-//using namespace boost;
 
+using namespace std;
+
+//Function declaratins
 void* thread_start_server(void *context);
 void routeMessage(string message, WebServer *server, int socketConnection);
 void send_404_error_to_client(WebServer *server, string version, int socketConnection);
 string getHeader(int content_length, string version, string status, int socketConnection);
 void execute_get_command(WebServer *server, string directory_path, string version, string status, int socketConnection);
 
+//Structure to old connection parameters
 struct connectionParams{
     string IPAddress;
     string PortNumber;
 };
 
+//Runs the server accepting the port that the server should be hosted on
 int main(int argc, char *argv[]) {
 
+    // Validate the correct number of arguments have been passed to the server
     if (argc < 2) {
         fprintf(stderr,"Usage: %s <port>\n", argv[0]);
         exit(1);
     }
 
-    pthread_t server_thread, client_thread;
+    //Create a thread for the server to run on
+    pthread_t server_thread;
     string portNumber = argv[1];
 
+    //sets up the connection params to be sent to the thread
     struct connectionParams params;
     params.PortNumber = portNumber;
 
+    //Creates the thread
     printf("\nSERVER MAIN: Starting server thread\n");
     pthread_create(&server_thread, 0, thread_start_server,&params);
 
+    //Join to prevent application from exiting
     printf("\nSERVER MAIN: Joining server thread\n");
     pthread_join(server_thread,0);
 
+    //End the program
     printf("\nSERVER MAIN: Ending Program\n");
     return 0;
 }
 
+//A function to be used by the thread create in order to host the server
 void *thread_start_server(void *context) {
+    //retreives the connecton params from the variable passed by the thread create
     struct connectionParams *params = (struct connectionParams*) context;
+
+    //start the server and host it on the specified port
     printf("SERVER: Constructing Web Server at IP:%s Port:%s", params->IPAddress.c_str(), params->PortNumber.c_str());
     WebServer* aserver = new WebServer(params->PortNumber);
+
+    //start listening and pass in the function pointer to the route message function
     printf("\nSERVER: Starting Listener\n");
     aserver->StartListening(&routeMessage);
     printf("\nSERVER: Finished Listening\n");
+
+    //if the server exits the listening loop, dispose of it properly
     delete aserver;
-    //aserver->~WebServer();
     printf("\nSERVER: Web Server Destroyed.\n");
 }
 
+//Overload of the get header method that assumes 200 OK is being passed back to the client
 string getHeader(int content_length, string version, int socketConnection)
 {
   return (getHeader(content_length, version, string("200 OK"), socketConnection));
 }
 
+//Primary get header function call is used to construct and return the header as a string
+//Since the header is sent back with each request this function is called in each response.
+//Version parameter is used to specify the response HTTP version
+//Content Length parameter is used to fill the content length header field
+//Status parameter is used to set the HTTP status like 200 OK or 404 Not Found
 string getHeader(int content_length, string version, string status, int socketConnection)
 {
+  //Construct the response version and status
   string response = "HTTP/" + version + " " + status +"\n";
+  //add server information
   response += "Server: simpleServer/1.0\n";
+
+  //construct the date for the response
   char buf[1000];
   time_t now = time(0);
   struct tm tm = *gmtime(&now);
   strftime(buf, sizeof(buf), "%a, %d %b %Y %H:%M:%S %Z", &tm);
   response += "Date: " + string(buf) + "\n";
+
+  //set the content type of the response
   response += "Content-type: text/html; charset=UTF-8\n";
+
+  //construct the content length portion of the resrponse.
   string length_string = boost::lexical_cast<std::string>(content_length);
   response += "Content-Length: " + length_string + "\n\n";
   return response;
 }
 
+//Sends a error 500 back to the client in any bad situation
+//If the http protocol is violated for example
 void send_500_error_to_client(WebServer *server, string version, int socketConnection)
 {
+  //Log error to output
   cout << "Server sending error to client 500" << endl;
+  //construct the response
   string response = "HTTP/" + version + " 500 Internal Server Error\n";
+  //Send the response back to the client
   server->WriteMessage(response, socketConnection);
 }
 
+//Overload of the get command assuming the return status is 200 OK
 void execute_get_command(WebServer *server, string directory_path, string version, int socketConnection)
 {
   return (execute_get_command(server, directory_path, version, "200 OK", socketConnection));
 }
 
+//Execturtes the get command
+//Server parameter is used to send the response to the client
+//Directory Path is used to load files form the server to send to the client
+//Version parameter is used to set the HTTP version in the header response
+//Status parameter is used to specify the HTTP response status
+//socketConnection parameter is used to sell the server which connection to send the response to
 void execute_get_command(WebServer *server, string directory_path, string version, string status, int socketConnection)
 {
+    //log the file being sent to the client on the server output
     printf("Server Sending File %s", directory_path.c_str());
+    //atempt to opent he file
     string line;
     ifstream myfile (directory_path.c_str());
+    //If file was opened then it was found
     if (myfile.is_open())
     {
+        //Load the file
         std::string file_contents((std::istreambuf_iterator<char>(myfile)), std::istreambuf_iterator<char>());
+        //Get the content length to populate in the header
         int content_length = file_contents.length();
+        //Get the header
         string header = getHeader(content_length, version, status, socketConnection);
+        //prepend the header and send it to the client
         server->WriteMessage(header + file_contents + "\n\0", socketConnection);
+        //close the file
         myfile.close();
     }
     else
     {
+      //If the file was not loaded and opened then send a 404 back to the client
       send_404_error_to_client(server, version, socketConnection);
     }
 }
 
+//Send 404 error back to the client by executing get command with a 404 status code for the file not found file
 void send_404_error_to_client(WebServer *server, string version, int socketConnection)
 {
   execute_get_command(server, string("html_root/file_not_found.html"), string("404 Not Found"), socketConnection);
 }
 
+//Deletes a file if it exists
+//Used for both the delete command and the put command
+//Directory path parameter is used to identify the file to be deleted
 void remove_if_exists(string directory_path)
 {
+    //open the file
     ifstream ifile(directory_path.c_str());
+    //if the file exists
     if (ifile)
     {
+        //delete the file
         printf("Removing file: %s\n",directory_path.c_str());
         remove(directory_path.c_str());
     }
 }
 
+//execture the put command
 void execute_put_command(WebServer *server, string directory_path, string message_body, string version, int socketConnection)
 {
+    //delete the file in anticipation of the put
     remove_if_exists(directory_path);
+    //write the file to the directory
     std::ofstream out(directory_path.c_str());
     out << message_body;
     out.close();
+    //return the confirmation to the client with the file created html in the return
     execute_get_command(server, string("html_root/file_created.html"), version, socketConnection);
 }
 
+//execture the delete command
 void execute_delete_command(WebServer *server, string directory_path, string version, int socketConnection)
 {
+    //Delete the file
     remove_if_exists(directory_path);
+    //return a delete success message back to the client
     execute_get_command(server, string("html_root/file_deleted.html"), version, socketConnection);
 }
 
+//execture the head command
 void execute_head_command(WebServer *server, string directory_path, string version, int socketConnection)
 {
+    //open the file
     ifstream myfile (directory_path.c_str());
+    //read the file to a string
     std::string file_contents((std::istreambuf_iterator<char>(myfile)), std::istreambuf_iterator<char>());
+    //get the content length
     int content_length = file_contents.length();
+    //construct the header
     string response = getHeader(content_length, version, socketConnection);
+    //write the response back to the client
     server->WriteMessage(response, socketConnection);
+    //close the file
     myfile.close();
 }
 
+//Routes messages that come from the web server object recieved from connected clients
 void routeMessage(string message, WebServer *server, int socketConnection)
 {
+    //reuqest has been recieved, log the unparsed message to the console
     printf("\nSERVER RECIEVED REQUEST: %s\n", message.c_str());
     vector <string> fields;
 
+    //split on new lines and get the first to isolate the request
+    //This is done specifically for the put command
     int first_return_index = message.find_first_of('\n');
     string request_only = message.substr(0, first_return_index);
+    //split the isolated request on spaces to seperate the request info
     boost::split( fields, request_only, boost::is_any_of( " " ));
 
+    //isolate the message body from the header
     int header_end_index = message.find("\n\n",0);
     string message_header = message.substr(0, header_end_index);
     string message_body = "";
@@ -172,6 +255,7 @@ void routeMessage(string message, WebServer *server, int socketConnection)
       message_body = message.substr(header_end_index + 2, message.length());
     }
 
+    //verify the proper formatting of the HTTP request
     int size = fields.size();
     if(size != 3)
     {
@@ -179,14 +263,16 @@ void routeMessage(string message, WebServer *server, int socketConnection)
       return;
     }
 
+    //assign variables to the command compoentns
     string command = fields[0];
     string directory = fields[1];
     string protocal_version = fields[2];
 
+    //split the protocol and version
     vector <string> fields2;
-
     boost::split (fields2, protocal_version, boost::is_any_of("//"));
 
+    //verify the protocol and version were properly formatted
     int size2 = fields2.size();
     if(size2 != 2)
     {
